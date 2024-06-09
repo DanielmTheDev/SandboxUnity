52d6c1ceb973d0f2fd4422a73355a3113ced7bc0
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-09 20:57:45 +0200

    Following

--- Assets/Scripts/SimpleEnemy.cs
+++ Assets/Scripts/SimpleEnemy.cs
@@ -24,12 +24,27 @@ public class SimpleEnemy : MonoBehaviour, IHittable
 
     private void Update()
     {
-        if (!_isDead)
+        if (IsPlayerInLineOfSight() && IsPlayerInRange())
         {
             _navMeshAgent.SetDestination(player.position);
         }
+        else
+        {
+            _navMeshAgent.SetDestination(gameObject.transform.position);
+        }
 
         _gravityApplier.ApplyGravity();
+    }
+
+    private bool IsPlayerInRange()
+        => Vector3.Distance(gameObject.transform.position, player.position) < 7f;
+
+    private bool IsPlayerInLineOfSight()
+    {
+        var isPlayerInFront = Vector3.Angle(transform.forward, player.position - transform.position) < 90f;
+        var hasHitSomething = Physics.Linecast(transform.position, player.position, out var hit);
+        var isVisionToPlayerClear = hasHitSomething && hit.collider.CompareTag("Player");
+        return isPlayerInFront && isVisionToPlayerClear;
     }
 
     public void OnHit()
@@ -40,22 +55,29 @@ public class SimpleEnemy : MonoBehaviour, IHittable
         }
 
         _isDead = true;
-        _navMeshAgent.isStopped = true;
+        ResetNavMeshAgent();
         StartCoroutine(RotateOverTime(rotationDuration, rotationAngle));
         Destroy(gameObject, 3f);
+    }
+
+    private void ResetNavMeshAgent()
+    {
+        _navMeshAgent.updateRotation = false;
+        _navMeshAgent.updatePosition = false;
+        _navMeshAgent.updateUpAxis = false;
+        _navMeshAgent.isStopped = true;
     }
 
     private IEnumerator RotateOverTime(float duration, float angle)
     {
         var elapsed = 0f;
-        var initialRotation = transform.rotation;
+        var initialRotation = pivot.transform.rotation;
         var targetRotation = initialRotation * Quaternion.Euler(0, 0, angle);
 
         while (elapsed < duration)
         {
             elapsed += Time.deltaTime;
             pivot.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsed / duration);
-            Debug.Log(pivot.transform.rotation);
             yield return null;
         }
 
