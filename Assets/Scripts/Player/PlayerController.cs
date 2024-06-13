44453e765595e9c8af9fd7d38f66b60628890fb5
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-13 16:44:58 +0200

    Player dying correctly

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,8 +15,10 @@ namespace Player
         private GravityApplier _gravityApplier;
         private PlayerInputHandler _inputHandler;
         private Animator _animator;
+        private InputActionMap _playerInput;
 
         private Vector3 _cameraOffset;
+        private bool _isDead;
 
         private void Awake()
         {
@@ -25,11 +27,18 @@ namespace Player
             _inputHandler = new(moveSpeed, rotationSpeed, _characterController, playerCamera, transform);
             _cameraOffset = playerCamera!.transform.transform.position - transform.position;
             _animator = GetComponent<Animator>();
+            _playerInput = GetComponent<PlayerInput>().actions.FindActionMap("Player");
         }
 
         public void Move(InputAction.CallbackContext context) => _inputHandler.SetMoveInput(context.ReadValue<Vector2>());
 
         public void Look(InputAction.CallbackContext context) => _inputHandler.SetMousePosition(context.ReadValue<Vector2>());
+
+        public void OnHit()
+        {
+            _playerInput.Disable();
+            _animator.SetTrigger(AnimatorExtensions.HitTrigger);
+        }
 
         private void Update()
         {
@@ -44,11 +53,6 @@ namespace Player
             var newCameraPosition = transform.position + _cameraOffset;
             newCameraPosition.y = playerCamera.transform.position.y;
             playerCamera.transform.position = newCameraPosition;
-        }
-
-        public void OnHit()
-        {
-            _animator.SetTrigger(AnimatorExtensions.HitTrigger);
         }
     }
 }
