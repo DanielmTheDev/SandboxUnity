000e643f71bbd7134d094c011b84f139b76cd25a
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-14 13:37:08 +0200

    Turn while shooting

--- Assets/Scripts/Enemy/AI/Shooting.cs
+++ Assets/Scripts/Enemy/AI/Shooting.cs
@@ -7,6 +7,7 @@ namespace Enemy
 {
     public class Shooting : IAiState
     {
+        private const float ROTATION_SPEED = 2f;
         private readonly NavMeshAgent _navMeshAgent;
         private readonly List<IAttacker> _attackers;
         private readonly Transform _agent;
@@ -26,7 +27,15 @@ namespace Enemy
         public void PerformUpdate()
         {
             _navMeshAgent.SetDestination(_agent.position);
+            RotateTowardsPlayer();
             _attackers.ForEach(attacker => attacker.Attack());
+        }
+
+        private void RotateTowardsPlayer()
+        {
+            var direction = (_player.position - _agent.position).normalized;
+            var lookRotation = Quaternion.LookRotation(new(direction.x, 0, direction.z));
+            _agent.rotation = Quaternion.Slerp(_agent.rotation, lookRotation, Time.deltaTime * ROTATION_SPEED);
         }
     }
 }
